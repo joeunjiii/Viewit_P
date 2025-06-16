8c01567cd8ebf11704f4a40ef3cf851bdb96f47f
smhrd <l0_y0k0@naver.com>
2025-06-17 00:52:44 +0900

    네이버 로그인 API 연동 - ver3

--- spring-server/src/main/java/com/example/viewit/springserver/config/WebConfig.java
+++ spring-server/src/main/java/com/example/viewit/springserver/config/WebConfig.java
@@ -1,18 +1,16 @@
+// src/main/java/com/example/viewit/springserver/config/WebConfig.java
 package com.example.viewit.springserver.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8083")
-                .allowedMethods("*")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")  // OPTIONS 포함
                 .allowCredentials(true);
-
     }
 }
