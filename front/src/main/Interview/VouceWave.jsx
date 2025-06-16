1a671e574d9aae56ab54ca9bddb44ef1672b1d20
yugwangmyeong <ykm5922@naver.com>
2025-06-16 13:47:07 +0900

    중간커밋

---
+++ front/src/main/Interview/VouceWave.jsx
@@ -0,0 +1,16 @@
+function VoiceWaveform() {
+    return (
+      <div className="voice-area">
+        <div className="voice-item">
+          <div className="voice-label">면접관</div>
+          <div className="waveform">파형1</div>
+        </div>
+        <div className="voice-item">
+          <div className="voice-label">면접자</div>
+          <div className="waveform">파형2</div>
+        </div>
+      </div>
+    );
+  }
+  export default VoiceWaveform;
+  
